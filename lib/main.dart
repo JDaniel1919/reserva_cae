885129b042f10a83d814812c691a12a6885c33c4
JDaniel1919 <jd.lopt1919@gmail.com>
2022-07-29 20:10:28 -0500

    V0.0.3 - SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // ignore: depend_on_referenced_packages
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 import 'Pantallas/Login.dart';
 import 'Pantallas/Principal.dart';
 import 'package:flutter/rendering.dart';
@@ -23,8 +24,45 @@ class MyApp extends StatelessWidget {
         primaryColorLight: Color(0xffb43f6b),
         primaryColorDark: Color(0xff4e001a),
         accentColor: Color(0xff9e9e9e),
+        appBarTheme: AppBarTheme(
+          color: Color(0xff800040),
+        ),
       ),
-      home: Principal(),
+      home: SplashSC(),
+    );
+  }
+}
+
+class SplashSC extends StatefulWidget {
+  SplashSC({Key? key}) : super(key: key);
+
+  @override
+  State<SplashSC> createState() => _SplashSCState();
+}
+
+class _SplashSCState extends State<SplashSC> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 5,
+      navigateAfterSeconds: Login(),
+      image: Image.asset("assets/LogoC.png"),
+      backgroundColor: Color(0xffffffff),
+      gradientBackground: LinearGradient(
+        begin: Alignment.topRight,
+        end: Alignment.bottomLeft,
+        colors: [
+          Colors.white,
+          Theme.of(context).primaryColor,
+        ],
+      ),
+      photoSize: 150,
+      styleTextUnderTheLoader: TextStyle(
+          fontSize: 18.0, fontWeight: FontWeight.bold,
+           color: Colors.white),
+      //loaderColor: Theme.of(context).primaryColor,
+      loaderColor: Colors.white,
+      loadingText: Text("Cargando..."),
     );
   }
 }
